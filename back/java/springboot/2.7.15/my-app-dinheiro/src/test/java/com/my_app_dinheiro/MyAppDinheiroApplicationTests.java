174a079766214d2bc1914693a33808f8ad385896
Gilvan2 <gilvan.silva.junior@gmail.com>
2024-11-26 15:36:38 -0300

    adição de repositório com springboot versão 3.4.0 funcional

---
+++ back/java/springboot/2.7.15/my-app-dinheiro/src/test/java/com/my_app_dinheiro/MyAppDinheiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my_app_dinheiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppDinheiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
